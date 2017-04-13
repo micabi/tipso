9b6d48d9f6a8029527a68cfa3c814616ce831954
Ueda <ueda_adachi20161001@yahoo.co.jp>
2017-04-13 14:06:26 +0900

    更新　ajax用のファイル

--- js/go.js
+++ js/go.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
-	//console.log("load");
-	$('.tipso').tipso({
-		background: 'rgba(0,0,0,0.6)',
+	$('.ruby').tipso({
+		background: 'rgba(82, 0, 43, 0.6)',
 		color: '#fff',
-		useTitle: false
+		useTitle: false,
+		titleBackground: 'rgba(49, 0, 26, 1)',
+		titleContent: 'ルビー',
+		ajaxContentUrl: 'gem/ruby.html'
 	});
+
+	$('.sapphire').tipso({
+		background: 'rgba(26, 13, 162, 0.6)',
+		color: '#fff',
+		useTitle: false,
+		titleBackground: 'rgba(20, 9, 126, 1)',
+		titleContent: 'サファイア',
+		ajaxContentUrl: 'gem/sapphire.html'
+	});
+
+	$('.emerald').tipso({
+		background: 'rgba(2, 55, 38, 0.6)',
+		color: '#fff',
+		useTitle: false,
+		titleBackground: 'rgba(0, 31, 21, 1)',
+		titleContent: 'エメラルド',
+		ajaxContentUrl: 'gem/emerald.html'
+	});
+
 });
